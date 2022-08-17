597b9e8825111e599a976f67083220be1746f545
EgorVertaev <eholife123@gmail.com>
2022-08-18 00:06:17 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,95 @@
+import "../scss/main.scss"
+import "../index.html"
+
+import Swiper from "swiper"
+
+
+const burger = document.querySelector('.menu__burger')
+const burgerInner = document.querySelector('.menu__burger-inner')
+const menuMob = document.querySelector('.menu-mob')
+const body = document.querySelector('body')
+const wrapper = document.querySelector('.wrapper')
+const menuMobBtn = document.querySelectorAll('.menu-mob__btn')
+const menuMobSub = document.querySelectorAll('.menu-mob__sub')
+
+menuMobBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    let submenu = button.nextElementSibling;
+    button.classList.toggle('menu-mob__btn--active')
+    submenu.classList.toggle('menu-mob__sub--active')
+  })
+})
+
+burger.addEventListener('click', (e) => {
+  e.stopPropagation();
+  burger.classList.toggle('menu__burger--active')
+  burgerInner.classList.toggle('menu__burger-inner--active')
+  body.classList.toggle('overlay')
+  menuMob.classList.toggle('menu-mob--active')
+})
+
+wrapper.addEventListener('click', () => {
+  body.classList.remove('overlay')
+  menuMob.classList.remove('menu-mob--active')
+  burger.classList.remove('menu__burger--active')
+  burgerInner.classList.remove('menu__burger-inner--active')
+  
+  menuMobBtn.forEach(button => {
+      button.classList.remove('menu-mob__btn--active')
+  })
+
+  menuMobSub.forEach( menu => {
+    menu.classList.remove('menu-mob__sub--active')
+  })
+})
+
+
+const swiper = new Swiper('.discond-slider', {
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  
+});
+
+
+const sliderNews = new Swiper('.slider-news__inner', {
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  
+});
+
+const sliderEmotions = new Swiper('.slider-emotions__inner', {
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 30,
+  
+});
+
+const sliderEvents = new Swiper('.slider-events__inner', {
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 30,
+  
+});
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, `src/js/main.js`),
+  output: {
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true,
+  },
+ 
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      
+    }),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`
+    }),
+  ],
+
+  devtool: isProd ? false : 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        include: /src/,
+        use: [ MiniCssExtractPlugin.loader,'css-loader', "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: `images/${filename('[ext]')}`
+        }
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: `fonts/${filename('[ext]')}`
+        }
+      },
+      // {
+        // test: /\.svg$/,
+        // loader: 'svg-sprite-loader',
+        // generator: {
+        //   filename: `sprite/${filename('[ext]')}`
+        // }
+      // }
+    ]
+  }
+}
